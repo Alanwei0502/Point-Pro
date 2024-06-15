41e788039e258a70c2c0361e270a00594f36ef6f
Alan Huang <alans40609@gmail.com>
2024-06-16 03:52:18 +0800

    vite.congif.ts

--- point-pro-fe/vite.config.ts
+++ point-pro-fe/vite.config.ts
@@ -6,10 +6,23 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx'],
     alias: {
       '~': resolve(__dirname, './src'),
     },
   },
   plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          mui: ['@mui/material'],
+          xDate: ['@mui/x-data-grid', '@mui/x-date-pickers'],
+          icon: ['@mui/icons-material'],
+          react: ['react', 'react-dom', 'react-router-dom'],
+          store: ['@reduxjs/toolkit', 'react-redux'],
+          utils: ['lodash', 'dayjs'],
+        },
+      },
+    },
+  },
 });
