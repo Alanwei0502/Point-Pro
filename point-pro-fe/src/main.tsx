39a98c6851dfeb5c6f2693625d13497d84971e38
Alan Huang <alans40609@gmail.com>
2024-06-17 01:40:56 +0800

    update react router

--- point-pro-fe/src/main.tsx
+++ point-pro-fe/src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import router from './router';
+import { App } from './router/App';
 import { store } from './store/store';
 import { theme } from './theme';
 import './index.css';
@@ -18,7 +17,7 @@ ReactDOM.createRoot(root).render(
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <CssBaseline />
-        <RouterProvider router={router} />
+        <App />
         <ToastContainer position='top-center' limit={3} hideProgressBar autoClose={1000} />
       </Provider>
     </ThemeProvider>
--- point-pro-fe/src/router/App.tsx
+++ point-pro-fe/src/router/App.tsx
@@ -1,40 +1,109 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import { Provider } from 'react-redux';
-import { theme } from '~/theme';
-import { store } from '~/store/store';
-import { pathObj } from '~/components';
+import { BrowserRouter, Navigate, Route, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom';
 import { Home } from '~/features/home';
-import { Booking } from '~/features/customer/booking';
-import { Menu } from '~/features/customer/menu';
 import { Login } from '~/features/admin/login';
+import { ProtectedRoute } from './ProtectedRoute';
 import { OrderManagement } from '~/features/admin/orderManagement';
 import { TakeOrder } from '~/features/admin/takeOrder';
 import { MenuManagement } from '~/features/admin/menuManagement';
 import { ReservationManagement } from '~/features/admin/reservationManagement';
-import { ProtectedRoute } from './protected';
+import { Booking } from '~/features/customer/booking';
+import { Menu } from '~/features/customer/menu';
+// import { PaymentCancel, PaymentConfirm } from '~/features/customer/payment';
+import { pathObj } from '~/components';
+import { Box } from '@mui/material';
 
 export const App = () => {
+  const router = createBrowserRouter(
+    [
+      // User
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: pathObj.booking,
+        element: <Booking />,
+      },
+      {
+        path: pathObj.menu,
+        element: <Menu />,
+      },
+      {
+        path: pathObj.payment,
+        children: [
+          // {
+          //   path: pathObj.confirm,
+          //   element: <PaymentConfirm />,
+          // },
+          // {
+          //   path: pathObj.cancel,
+          //   element: <PaymentCancel />,
+          // },
+        ],
+      },
+      // Admin
+      {
+        path: pathObj.admin,
+        element: <Login />,
+      },
+      {
+        path: pathObj.admin,
+        element: <ProtectedRoute />,
+        children: [
+          {
+            path: pathObj.orderManagement,
+            element: <OrderManagement />,
+          },
+          {
+            path: pathObj.takeOrder,
+            element: <TakeOrder />,
+          },
+          {
+            path: pathObj.reservationMangement,
+            element: <ReservationManagement />,
+          },
+          {
+            path: pathObj.menuManagement,
+            element: <MenuManagement />,
+          },
+        ],
+      },
+      {
+        path: `${pathObj.admin}/*`,
+        element: <Navigate to='.' replace />,
+      },
+    ],
+    {
+      basename: '/',
+    },
+  );
+
+  // return <RouterProvider router={router} />;
+
   return (
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path={pathObj.booking} element={<Booking />} />
-            <Route path={pathObj.menu} element={<Menu />} />
-            {/* <Route path={pathObj.payment} /> */}
-            <Route path={pathObj.admin} element={<Login />} />
-            <Route path={pathObj.admin} element={<ProtectedRoute />}>
-              <Route path={pathObj.orderManagement} element={<OrderManagement />} />
-              <Route path={pathObj.takeOrder} element={<TakeOrder />} />
-              <Route path={pathObj.menuManagement} element={<MenuManagement />} />
-              <Route path={pathObj.reservationMangement} element={<ReservationManagement />} />
-            </Route>
-            <Route path='/*' element={<Navigate to='/' replace />} />
-          </Routes>
-        </BrowserRouter>
-      </Provider>
-    </ThemeProvider>
+    <BrowserRouter basename='/'>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path={pathObj.booking} element={<Booking />} />
+        <Route path={pathObj.menu} element={<Menu />} />
+        {/* <Route path={pathObj.payment} /> */}
+        <Route path={pathObj.admin} element={<Login />} />
+        <Route path={pathObj.admin} element={<ProtectedRoute />}>
+          <Route path={pathObj.orderManagement} element={<OrderManagement />} />
+          <Route path={pathObj.takeOrder} element={<TakeOrder />} />
+          <Route path={pathObj.menuManagement} element={<MenuManagement />} />
+          <Route path={pathObj.reservationMangement} element={<ReservationManagement />} />
+          <Route
+            path='*'
+            element={
+              <Box display='flex' height='100%' alignItems='center' justifyContent='center'>
+                無此頁面
+              </Box>
+            }
+          />
+        </Route>
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
