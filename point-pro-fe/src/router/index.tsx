689e695437216a5e341cced33c3f837606b16e00
Alan Huang <alans40609@gmail.com>
2024-06-16 19:41:26 +0800

    update react router

--- point-pro-fe/src/router/index.tsx
+++ point-pro-fe/src/router/index.tsx
@@ -12,62 +12,67 @@ import { Menu } from '~/features/customer/menu';
 import { pathObj } from '~/components';
 
 const router = createBrowserRouter([
-  // User
   {
     path: '/',
-    element: <Home />,
-  },
-  {
-    path: pathObj.booking,
-    element: <Booking />,
-  },
-  {
-    path: pathObj.menu,
-    element: <Menu />,
-  },
-  {
-    path: pathObj.payment,
     children: [
-      // {
-      //   path: pathObj.confirm,
-      //   element: <PaymentConfirm />,
-      // },
-      // {
-      //   path: pathObj.cancel,
-      //   element: <PaymentCancel />,
-      // },
+      // User
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: pathObj.booking,
+        element: <Booking />,
+      },
+      {
+        path: pathObj.menu,
+        element: <Menu />,
+      },
+      {
+        path: pathObj.payment,
+        children: [
+          // {
+          //   path: pathObj.confirm,
+          //   element: <PaymentConfirm />,
+          // },
+          // {
+          //   path: pathObj.cancel,
+          //   element: <PaymentCancel />,
+          // },
+        ],
+      },
+      // Admin
+      {
+        path: pathObj.admin,
+        element: <Login />,
+      },
+      {
+        path: pathObj.admin,
+        element: <ProtectedRoute />,
+        children: [
+          {
+            path: pathObj.orderManagement,
+            element: <OrderManagement />,
+          },
+          {
+            path: pathObj.takeOrder,
+            element: <TakeOrder />,
+          },
+          {
+            path: pathObj.reservationMangament,
+            element: <ReservationManagement />,
+          },
+          {
+            path: pathObj.menuManagament,
+            element: <MenuManagement />,
+          },
+        ],
+      },
+      {
+        path: `${pathObj.admin}/*`,
+        element: <Navigate to='.' replace />,
+      },
     ],
-  },
-  // Admin
-  {
-    path: pathObj.admin,
-    element: <Login />,
-  },
-  {
-    path: pathObj.admin,
-    element: <ProtectedRoute />,
-    children: [
-      {
-        path: pathObj.orderManagement,
-        element: <OrderManagement />,
-      },
-      {
-        path: pathObj.takeOrder,
-        element: <TakeOrder />,
-      },
-      {
-        path: pathObj.reservationMangament,
-        element: <ReservationManagement />,
-      },
-      {
-        path: pathObj.menuManagament,
-        element: <MenuManagement />,
-      },
-    ],
-  },
-  {
-    path: `${pathObj.admin}/*`,
-    element: <Navigate to='.' replace />,
   },
 ]);
 
