d486358a29f14ea47eae57f96bd6b4f73dfada8d
Alan Huang <alans40609@gmail.com>
2024-07-03 11:46:10 +0800

    optimize bundle size

--- point-pro-fe/vite.config.ts
+++ point-pro-fe/vite.config.ts
@@ -15,17 +15,52 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          muiStyle: ['@mui/material/styles', '@mui/material/CssBaseline'],
-          mui: ['@mui/material'],
-          xDate: ['@mui/x-date-pickers'],
-          xGrid: ['@mui/x-data-grid'],
-          icon: ['@mui/icons-material'],
-          react: ['react', 'react-dom', 'react-router-dom'],
-          store: ['@reduxjs/toolkit', 'react-redux'],
-          utils: ['lodash', 'dayjs', 'qrcode.react', 'react-toastify'],
-          dnd: ['@dnd-kit/core', '@dnd-kit/modifiers', '@dnd-kit/sortable', '@dnd-kit/utilities'],
+        manualChunks: (id) => {
+          if (id.includes('@mui/material')) {
+            return 'mui-' + id.split('/').slice(-1)[0];
+          }
+          if (id.includes('@mui/icons-material')) {
+            return 'mui-icons';
+          }
+          if (id.includes('@mui/x-date-pickers')) {
+            return 'xDate';
+          }
+          if (id.includes('@mui/x-data-grid')) {
+            return 'xGrid';
+          }
+          if (id.includes('react')) {
+            return 'react';
+          }
+          if (id.includes('@reduxjs/toolkit')) {
+            return 'store';
+          }
+          if (id.includes('lodash')) {
+            return 'lodash';
+          }
+          if (id.includes('dayjs')) {
+            return 'dayjs';
+          }
+          if (id.includes('qrcode.react')) {
+            return 'qrcode';
+          }
+          if (id.includes('react-toastify')) {
+            return 'react-toastify';
+          }
+          if (id.includes('@dnd-kit')) {
+            return 'dnd';
+          }
         },
+        // {
+        // muiStyle: ['@mui/material/styles', '@mui/material/CssBaseline'],
+        // mui: ['@mui/material'],
+        // xDate: ['@mui/x-date-pickers'],
+        // xGrid: ['@mui/x-data-grid'],
+        // icon: ['@mui/icons-material'],
+        // react: ['react', 'react-dom', 'react-router-dom'],
+        // store: ['@reduxjs/toolkit', 'react-redux'],
+        // utils: ['lodash', 'dayjs', 'qrcode.react', 'react-toastify'],
+        // dnd: ['@dnd-kit/core', '@dnd-kit/modifiers', '@dnd-kit/sortable', '@dnd-kit/utilities'],
+        // },
       },
     },
   },
